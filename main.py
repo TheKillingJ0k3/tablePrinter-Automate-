9801fc7bc6e7677fcc6ada643e6660267d7eb08d
George Pt <angry_sephiroth@hotmail.com>
2019-03-02 04:39:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,38 +4,41 @@ tableData = [['apples', 'oranges', 'cherries', 'banana'],
 
 def printTable(list):
     colWidths = [0] * len(list)
-
+    for i in range(len(list)):
+        for j in range(len(list[0])):
+            colWidths[i] = len(list[i][j])
+            if colWidths[i] < len(list[i][j-1]):
+                colWidths[i] = len(list[i][j-1])
+        
     # Αποθηκεύω το width της κάθε λέξης σε μεταβλητές, τις περνάω σε μια λίστα, τη σορτάρω και παίρνω την υψηλότερη τιμή.
     #Παράδειγμα για την πρώτη γραμμή του tableData. (Πιο εύκολος τρόπος να συγκρίνω μεταβλητές και να πάρω τη μεγαλύτερη;)
-    a0 = len(list[0][0])
-    b0 = len(list[0][1])
-    c0 = len(list[0][2])
-    d0 = len(list[0][3])
-    sumList = [a0, b0, c0, d0]
-    sumList.sort()
-    colWidths[0] = sumList[-1]
-    print(colWidths[0])
+    # a0 = len(list[0][0])
+    # b0 = len(list[0][1])
+    # c0 = len(list[0][2])
+    # d0 = len(list[0][3])
+    # sumList = [a0, b0, c0, d0]
+    # sumList.sort()
+    # colWidths[0] = sumList[-1]
+    # print(colWidths[0])
 
     #Δεύτερη γραμμή:
-    a1 = len(list[1][0])
-    b1 = len(list[1][1])
-    c1 = len(list[1][2])
-    d1 = len(list[1][3])
-    sumList = [a1, b1, c1, d1]
-    sumList.sort()
-    colWidths[1] = sumList[-1]
-    print(colWidths[1])
-
+    # a1 = len(list[1][0])
+    # b1 = len(list[1][1])
+    # c1 = len(list[1][2])
+    # d1 = len(list[1][3])
+    # sumList = [a1, b1, c1, d1]
+    # sumList.sort()
+    # colWidths[1] = sumList[-1]
+    # print(colWidths[1])
     #Τρίτη γραμμή:
-    a2 = len(list[2][0])
-    b2 = len(list[2][1])
-    c2 = len(list[2][2])
-    d2 = len(list[2][3])
-    sumList = [a2, b2, c2, d2]
-    sumList.sort()
-    colWidths[2] = sumList[-1]
-    print(colWidths[2])
-
+    # a2 = len(list[2][0])
+    # b2 = len(list[2][1])
+    # c2 = len(list[2][2])
+    # d2 = len(list[2][3])
+    # sumList = [a2, b2, c2, d2]
+    # sumList.sort()
+    # colWidths[2] = sumList[-1]
+    # print(colWidths[2])
     #Αυτό πρέπει να γίνει αυτόματα για κάθε γραμμή του tableData.
     #Το πρόβλημά μου είναι ότι σε loop αντικαθίστανται οι μεταβλητές συνεχώς και δεν αποθηκεύονται εκτός loop αυτές που θέλω.
 
